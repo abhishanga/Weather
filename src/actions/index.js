a445679b4d492e64c69a75b17d021745421c20b3
Abhishanga Upadhyay <abhishangaupadhyay@Abhishangas-MacBook-Pro.local>
2016-11-08 22:42:30 -0800

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = 'af8773f50f321fad62a9dabb1383e9cf';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
